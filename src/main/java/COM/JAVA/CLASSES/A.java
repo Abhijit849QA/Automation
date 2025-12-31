3f73ddb4b056e453a13fbc34f0747be9300e14b6
Abhi <hp@Abhisek>
2025-12-31 14:25:49 +0300

    merge commit of clss A from f1

--- src/main/java/COM/JAVA/CLASSES/A.java
+++ src/main/java/COM/JAVA/CLASSES/A.java
@@ -3,11 +3,9 @@ package COM.JAVA.CLASSES;
 public class A {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		// TODO Auto-generated method stub
-=======
+
 		System.out.println("hii");
->>>>>>> f1
+
 
 	}
 
